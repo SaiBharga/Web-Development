f7ecd059c0d9ae9b814b2a5ce193f89b33b8814a
Sai Bhargavi <95035534+SaiBharga@users.noreply.github.com>
2021-12-14 19:30:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome | Aqgromalin Farmtech Services Private Limited</title>
+
+    <?php
+    include "includes/head_links.php";
+    ?>
+    <link href="css/home.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+    include "includes/header.php";
+    ?>
+    <?php
+    include "includes/signup_modal.php";
+    include "includes/login_modal.php";
+    include "includes/footer.php";
+    ?>
+
+</body>
+
+</html>
