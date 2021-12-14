30d81fea0a177ce11203d64741f44136e5958ffb
Sai Bhargavi <95035534+SaiBharga@users.noreply.github.com>
2021-12-14 19:35:37 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-copyright">© 2020 Copyright Aqgromalin Farmtech Services Private Limited </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
